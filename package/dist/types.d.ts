3295cb5d25e396d504089a446b8e52fdcb846b42
Harshil Pradhan <harshil.p@manifestux.com>
2023-04-22 17:07:41 +0530

    Add build

---
+++ package/dist/types.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="react" />
+export interface DateRange {
+    startDate?: Date;
+    endDate?: Date;
+}
+export type Setter<T> = React.Dispatch<React.SetStateAction<T>> | ((value: T) => void);
+export declare enum NavigationAction {
+    Previous = -1,
+    Next = 1
+}
+export type DefinedRange = {
+    startDate: Date;
+    endDate: Date;
+    label: string;
+};
